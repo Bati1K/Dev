2b0837c1be6d7e619eade1bc04502ac0a7552c2b
Bati1K <131696253+Bati1K@users.noreply.github.com>
2023-04-27 18:49:09 -0500

    update with breaks

--- AvgSalary.js
+++ AvgSalary.js
@@ -20,9 +20,26 @@ let salary;
 
 // your code goes here
 switch (education) {
-  case "no high school diploma":
-  salary = 25636;
-  case "high school diploma":
-  salary = 35256;
+      case "no high school diploma":
+        salary = 25636;
+        break;
+    case "high school diploma":
+        salary = 35256;
+        break;
+    case "Associate's degree":
+        salary = 41496;
+        break;
+    case "Bachelor's degree":
+        salary = 59124;
+        break;
+    case "Master's degree":
+        salary = 69732;
+        break;
+    case "Professional degree":
+        salary = 89960;
+        break;
+    case "Doctoral degree":
+        salary = 84396;
+        break;
 } 
 console.log("In 2015, a person with a "+ education + " earned an average of $"+ salary.toLocaleString("en-US") + "/year.");
