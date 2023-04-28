4ea60ad07858334ddcdff633c1faafd5506afb0d
Bati1K <131696253+Bati1K@users.noreply.github.com>
2023-04-28 08:18:51 -0500

    quiz files

--- Quizzes/BankAccountBalance.js
+++ Quizzes/BankAccountBalance.js
@@ -1,12 +1,12 @@
 /*
-* QUIZ REQUIREMENTS
-* 1. Your code should have the variables `balance`, `checkBalance`, `isActive`
-* 2. Your code should include an `if...else` conditional statement
-* 3. Your code should produce the expected output
-* 4. Your code should not be empty
-* 5. BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED. 
-*/
-
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `balance`, `checkBalance`, `isActive`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should produce the expected output
+ * 4. Your code should not be empty
+ * 5. BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED. 
+ */
+ 
 // change the values of `balance`, `checkBalance`, and `isActive` to test your code
 const balance = 100;
 const checkBalance = true;
