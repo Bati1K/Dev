8c5f6819ca10f49321f6f278dc56bdfcd1a3b326
Bati1K <131696253+Bati1K@users.noreply.github.com>
2023-05-02 18:23:46 -0500

    building crew with array

---
+++ Quizzes/FlighCrewArray.js
@@ -0,0 +1,21 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `crew`
+ * - The variable `crew` should be an array containing the Serenity\'s crew
+ * - Your code should print `crew` to the console as an array. Do not iterate over the elements. 
+ */
+ 
+
+// your code goes here
+const crew = [captain = "Mal", 
+              second = "Zoe", 
+              pilot = "Wash",
+              companion = "Inara",
+              mercenary = "Jayne",
+              mechanic = "Kaylee"];
+
+console.log(crew);
