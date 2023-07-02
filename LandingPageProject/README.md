e7545e780bb993d99f8f37129d6ef965332cfd3d
Bati Kadir <131696253+Bati1K@users.noreply.github.com>
2023-07-02 07:42:10 -0500

    Project upload

---
+++ LandingPageProject/README.md
@@ -0,0 +1,27 @@
+# Project Title: Landing Page Project
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. Student will need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+
+## Feautes
+
+**Navigation Bar with Active Functionality**: The website includes a dynamic navigation bar that highlights the active page or section, providing users with clear visual cues for their current location within the website.
+
+**Reactive Page**: The website employs a reactive design approach, ensuring that content and layout adapt seamlessly to different screen sizes and devices. Whether accessed from a desktop, tablet, or mobile device, users will enjoy a consistent and optimized browsing experience.
+
+**Responsive Design**: The website is designed with a responsive layout that automatically adjusts and scales elements based on the screen size and resolution. This guarantees that the content remains easily readable and accessible, regardless of the device used to access it.
+
+## Technology Used
+
+**Front-End**: HTML, CSS, JavaScript
+
+## Usage
+
+**Navigation Bar**: The navigation bar at the top of the website provides a menu of sections or pages within the site.
+Scrolling to Specific Sections: To navigate to a specific section, simply click on the corresponding menu item in the navigation bar.
+
+## Contact
+
+If you have any questions, suggestions, or feedback, please feel free to contact me at **Batisani16@gmail.com**
---
+++ LandingPageProject/js/app.js
@@ -0,0 +1,100 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+const sections = document.querySelectorAll("section");
+const navbarList = document.getElementById("navbar__list");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+let ulElement = document.querySelector('#navbar__list');
+// Array of content for the li elements
+let sectionContent = ['Section 1', 'Section 2', 'Section 3', 'Section 4'];
+
+// Loop through the array and create li elements
+for (let i = 0; i < sectionContent.length; i++) {
+  // Create a new li element
+  let liElement = document.createElement('li');
+  // Set the content of the li element
+  liElement.textContent = sectionContent[i];
+  // Add the li elements to the ul element
+  ulElement.appendChild(liElement);
+
+  liElement.addEventListener('click', scrollToSection);
+}
+
+// Add class 'active' to section when near top of viewport
+function setActiveSection() {
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top >= 0 && rect.top <= window.innerHeight * 0.5) {
+      section.classList.add("active");
+    } else {
+      section.classList.remove("active");
+    }
+  });
+}
+
+// Scroll to anchor ID using scrollTO event
+// Function to scroll to the corresponding section when a li element is clicked
+function scrollToSection(event) {
+    // Get the data-nav attribute value of the clicked li element
+    let navItem = event.target.textContent;
+    // Remove spaces and convert to lowercase to match the section ID
+    let sectionId = navItem.replace(/\s+/g, '').toLowerCase();
+    // Scroll to the section with the corresponding ID
+    let section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+
+// Scroll to section on link click
+function scrollToSection(event) {
+  event.preventDefault();
+  const targetSectionId = event.target.textContent.replace(/\s+/g, "").toLowerCase();
+  const targetSection = document.getElementById(targetSectionId);
+  targetSection.scrollIntoView({ behavior: "smooth" });
+}
+// Set sections as active
+window.addEventListener("scroll", setActiveSection);
+
